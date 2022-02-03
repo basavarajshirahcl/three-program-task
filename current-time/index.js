8658fb38c1c06440229d74decb37d097488cf5a4
basavarajshirahcl <basavaraj.shira@hcl.com>
2022-02-03 13:29:04 +0530

    Adding cron service on time update

--- current-time/index.js
+++ current-time/index.js
@@ -1,9 +1,19 @@
 const express = require('express');
+const CronJob = require('cron').CronJob;
+const fs = require('fs');
 const app = express()
 
 app.get('/', (req, res) => {
-    res.send(<currentTime>${new Date()}</currentTime>);
+    let time = fs.readFileSync('./data.txt', 'utf8');
+    res.send(`<currentTime>${time}!!</currentTime>`);
 });
+
+const job = new CronJob('*/8 * * * * *', function () {
+    fs.writeFileSync('./data.txt', `${new Date()}`, { flag: 'w' });
+
+}, null, true, 'America/Los_Angeles');
+job.start();
+
 
 app.listen(5000, () => {
     console.log('Listing on 5000');
