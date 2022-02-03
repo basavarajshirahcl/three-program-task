37230a03fb693e68306d546a617851924c22ec6c
basavarajshirahcl <98102366+basavarajshirahcl@users.noreply.github.com>
2022-02-03 16:33:13 +0530

    Update index.js

--- current-time/index.js
+++ current-time/index.js
@@ -3,18 +3,26 @@ const CronJob = require('cron').CronJob;
 const fs = require('fs');
 const app = express()
 
-app.get('/', (req, res) => {
-    let time = fs.readFileSync('./data.txt', 'utf8');
-    res.send(`<currentTime>${time}!!</currentTime>`);
-});
-
+//Update time every 8sec
 const job = new CronJob('*/8 * * * * *', function () {
     fs.writeFileSync('./data.txt', `${new Date()}`, { flag: 'w' });
 
 }, null, true, 'America/Los_Angeles');
 job.start();
 
+/**
+ * Returns the time 
+ * @param {any} req
+ * @param {any} res
+ * @returns {string}
+ */
+app.get('/', (req, res) => {
+    let time = fs.readFileSync('./data.txt', 'utf8');
+    res.send(`<currentTime>${time}!!</currentTime>`);
+});
 
+
+//server listing on 5000 port number
 app.listen(5000, () => {
     console.log('Listing on 5000');
 });
